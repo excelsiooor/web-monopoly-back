27d1e5b0e02ca8962b4969000bab85bd804b554c
excelsiooor <homenich101@gmail.com>
2023-03-25 11:35:28 +0200

    rename directory

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,13 @@
+import { IsString } from "class-validator";
+
+export class UserDTO {
+
+  @IsString()
+  displayName: string;
+
+  @IsString()
+  email: string;
+
+  @IsString()
+  photo: string;
+}
---
+++ src/user/user.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/libs/entities/user.entity';
+import { AuthController } from './user.controller';
+import { GoogleService } from './services/google.service';
+import { AuthService } from './services/auth.service';
+
+const userRepository = TypeOrmModule.forFeature([UserEntity])
+
+@Module({
+  imports: [userRepository],
+  controllers: [AuthController],
+  providers: [GoogleService, {
+    provide: 'AUTH_SERVICE',
+    useClass: AuthService,
+  }],
+})
+export class AuthModule {}
