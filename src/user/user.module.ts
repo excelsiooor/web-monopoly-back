d910f58140db44bee078f9d026572f3a801dfdba
excelsiooor <homenich101@gmail.com>
2023-03-28 18:57:36 +0300

    provide all modules

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { UserEntity } from 'src/libs/entities/user.entity'
-import { AuthController } from './user.controller'
+import { UserController } from './user.controller'
 import { AuthService } from './services/auth.service'
 import { TokenService } from './services/jwt.service'
 import { JwtModule } from '@nestjs/jwt'
+import { UserService } from './services/user.service'
+import { RoomsEntity } from 'src/libs/entities/room.entity'
 
 const userRepository = TypeOrmModule.forFeature([UserEntity])
+const roomsRepository = TypeOrmModule.forFeature([RoomsEntity])
 
 const JWTModule = JwtModule.registerAsync({
   useFactory: () => ({
@@ -15,8 +18,8 @@ const JWTModule = JwtModule.registerAsync({
 })
 
 @Module({
-  imports: [userRepository, JWTModule],
-  controllers: [AuthController],
-  providers: [TokenService, AuthService],
+  imports: [userRepository, roomsRepository, JWTModule],
+  controllers: [UserController],
+  providers: [TokenService, AuthService, UserService],
 })
 export class UserModule {}
