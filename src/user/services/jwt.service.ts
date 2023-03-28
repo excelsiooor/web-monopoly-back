683608b5bcc822e0d1b67e18072b749512d9b01d
excelsiooor <homenich101@gmail.com>
2023-03-28 18:57:13 +0300

    rework jwt service

--- src/user/services/jwt.service.ts
+++ src/user/services/jwt.service.ts
@@ -1,38 +1,19 @@
 import { Injectable } from '@nestjs/common'
-
 import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class TokenService {
   constructor(private readonly jwtService: JwtService) {}
 
-  generateToken(email: string) {
+  generateTokens(email: string) {
     const accessToken = this.jwtService.sign({ data: { email } }, { expiresIn: '12h' })
-    const refreshToken = this.jwtService.sign({ data: { email } }, { expiresIn: '1d' })
+    const refreshToken = this.jwtService.sign({ data: { email } }, { expiresIn: '7d' })
     return { accessToken, refreshToken }
   }
 
   verifyToken(token: string) {
-    try {
-      const verifyedToken = this.jwtService.verify(token)
+    const verifyedToken = this.jwtService.verify(token)
 
-      if (!verifyedToken) {
-        return { message: 'Invalid Token' }
-      }
-
-      return token
-    } catch (error) {
-      return { error }
-    }
-  }
-
-  refreshToken(refreshToken: string) {
-    try {
-      const verifyedToken = this.jwtService.verify(refreshToken)
-
-      return { email: verifyedToken.data.email }
-    } catch (error) {
-      return { error }
-    }
+    return { email: verifyedToken.data.email }
   }
 }
