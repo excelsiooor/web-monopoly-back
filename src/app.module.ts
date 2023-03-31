0951024018120aceeb4b21fe4f08ca39e42b78c4
excelsiooor <homenich101@gmail.com>
2023-03-31 11:03:55 +0300

    rework configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { TypeOrmModule } from '@nestjs/typeorm'
-
-import { UserEntity } from './libs/entities/user.entity'
-import { RoomsEntity } from './libs/entities/room.entity'
+import { postgreModule } from './config/postgre-db.config'
 import { RoomsModule, UserModule } from './modules'
 
 const dotEnvConfig = ConfigModule.forRoot({ envFilePath: '.env' })
-
-const postgreModule = TypeOrmModule.forRoot({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'root',
-  password: 'root',
-  database: 'web-monopoly',
-  entities: [UserEntity, RoomsEntity],
-  synchronize: true,
-})
 
 @Module({
   imports: [dotEnvConfig, postgreModule, UserModule, RoomsModule],
